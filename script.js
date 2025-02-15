7f3df899df2c28d93785a1fa43945934b650bd61
thile <nothilecmhlanga@outlook.com>
2025-02-15 22:56:05 +0800

    adding images

---
+++ script.js
@@ -0,0 +1,53 @@
+const body = document.querySelector('body');
+const products = document.querySelector('.products');
+
+
+//Add an empty array to store the cart items
+let ArrProducts = [
+    {
+        id: 1,
+        name: 'Product 1',
+        Image: 'product1.jpg',
+        price: 100,
+        rating: 4,
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        Image: 'product2.jpg',
+        price: 80,
+        rating: 3.5,
+    }, 
+    {
+        id: 3,
+        name: 'Product 3',
+        Image: 'product3.jpg',
+        price: 50,
+        rating: 2,
+    },
+    {
+        id: 4,
+        name: 'Product 4',
+        Image: 'product4.jpg',
+        price: 150,
+        rating: 4.5,
+   },
+    {
+        id: 5,
+        name: 'Product 5',
+        Image: 'product5.jpg',
+        price: 200,
+        rating: 5,
+    },
+    {
+        id: 6,
+        name: 'Product 6',
+        Image: 'product6.jpg',
+        price: 35,
+        rating: 2.5,
+    },
+    
+
+];
+
+s
